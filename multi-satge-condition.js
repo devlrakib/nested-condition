a0c907fc795553669ccb9986015821506c4d0386
devlrakib <devrakib5@gmail.com>
2024-03-14 05:07:02 +0600

    first commit

---
+++ multi-satge-condition.js
@@ -0,0 +1,11 @@
+var money = 100;
+var danishPrice = 45;
+var butterbread = 35;
+var toastbuiscuits = 20;
+
+if(danishPrice < money){
+    console.log("This is a kafasfkfs");
+}
+else if(butterbread < money){
+   console.log("The Butterbread");
+}
---
+++ nested-condition.js
@@ -0,0 +1,20 @@
+var math = true;
+var geometry = true;
+var straightLight = false;
+
+if (math==true){
+    if (geometry==true){
+        if (straightLight==true){
+
+        }
+        else{
+            console.log("Baka pothe colba na");
+        }
+    }
+    else{
+
+    }
+}
+else{
+
+}
